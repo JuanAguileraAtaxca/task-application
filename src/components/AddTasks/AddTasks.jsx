d89c90badc316c22fa94b6522cdfc149a7911f05
Juan Jose Aguilera <aguileraataxca@gmail.com>
2022-08-21 01:40:28 -0500

    updating

--- src/components/AddTasks/AddTasks.jsx
+++ src/components/AddTasks/AddTasks.jsx
@@ -1,8 +1,10 @@
 import style from './AddTasks.module.css'; 
+
 const AddTasks = () => {
 
     return(
         <form className={style.AddTasksContainer}>
+            {/* this token is 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 */ }
             <h2 className={style.AddTasksTitle + " fontUbuntu"}> Add a new task </h2>
             <input className={style.AddTasksInput + " fontUbuntu"} placeholder="Add your task" type="text"/>
             <input className={style.AddTasksButton + " fontUbuntu"} type="submit" value="add tasks" />
